9d4f6ec0b97e2f3ce9c0757f68239a046f5219b0
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2016-02-20 23:20:29 -0500

    Reindent
    reindenting

--- ShortList/UIApplication.swift
+++ ShortList/UIApplication.swift
@@ -9,19 +9,19 @@ import UIKit
 
 extension UIApplication {
     
-    class func topViewController(viewController: UIViewController? = UIApplication.sharedApplication().keyWindow?.rootViewController) -> UIViewController? {
-        if let nav = viewController as? UINavigationController {
-            return topViewController(nav.visibleViewController)
-        }
-        if let tab = viewController as? UITabBarController {
-            if let selected = tab.selectedViewController {
-                return topViewController(selected)
-            }
-        }
-        if let presented = viewController?.presentedViewController {
-            return topViewController(presented)
-        }
-        
-        return viewController
+  class func topViewController(viewController: UIViewController? = UIApplication.sharedApplication().keyWindow?.rootViewController) -> UIViewController? {
+    if let nav = viewController as? UINavigationController {
+      return topViewController(nav.visibleViewController)
     }
+    if let tab = viewController as? UITabBarController {
+      if let selected = tab.selectedViewController {
+        return topViewController(selected)
+      }
+    }
+    if let presented = viewController?.presentedViewController {
+      return topViewController(presented)
+    }
+    
+    return viewController
+  }
 }
--- ShortList/UIView.swift
+++ ShortList/UIView.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 extension UIView {
-    class func loadNib<T: UIView>(viewType: T.Type) -> T {
-        let className = String.className(viewType)
-        return NSBundle(forClass: viewType).loadNibNamed(className, owner: nil, options: nil).first as! T
-    }
-    
-    class func loadNib() -> Self {
-        return loadNib(self)
-    }
+  class func loadNib<T: UIView>(viewType: T.Type) -> T {
+    let className = String.className(viewType)
+    return NSBundle(forClass: viewType).loadNibNamed(className, owner: nil, options: nil).first as! T
+  }
+  
+  class func loadNib() -> Self {
+    return loadNib(self)
+  }
 }
