493b94c644b901942d0c365af528260b911762e5
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2016-09-03 09:35:05 -0400

    Forgot to add this to previous commit
    MapPIn goes with previous commit.

--- ShortList/LocationMapPin.swift
+++ ShortList/LocationMapPin.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2016 Andrew Steinmeyer. All rights reserved.
 //
 
-import Foundation
+import MapKit
+
+class LocationMapPin: NSObject, MKAnnotation {
+  let title: String?
+  let coordinate: CLLocationCoordinate2D
+  
+  init(title: String, coordinate: CLLocationCoordinate2D) {
+    self.title = title
+    self.coordinate = coordinate
+    
+    super.init()
+  }
+  
+}
